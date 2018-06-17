6817d27581d0a3a58864d36a6a7794bd11f0b999
Ritwik Das <ritwikdas54@gmail.com>
2018-06-16 18:23:11 -0700

    Problem No 155

---
+++ 155.py
@@ -0,0 +1,28 @@
+'''
+Given a list of elements, find the majority element, which appears more than half the times (> floor(len(lst) / 2.0)).
+
+You can assume that such element exists.
+
+For example, given [1, 2, 1, 1, 3, 4, 1], return 1.
+'''
+
+
+def solve(arr):
+	votes = 0
+	number = None
+	for ele in arr:
+		if votes == 0:
+			number = ele
+			votes+=1
+
+		elif ele!=number:
+			votes-=1
+		elif ele==number:
+			votes+=1
+		print(number, votes)
+
+	return number
+
+if __name__=='__main__':
+	arr = [1, 2, 1, 1, 3, 4, 1]
+	print(solve(arr))
